48fd543d3d0ef0b3e5158577e862bf7ccd749087
Joan Senent <joansenent@hotmail.com>
2021-06-30 21:21:37 +0200

    Uploaded all files

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+
+$(document).ready(function() {
+
+	// image
+
+	// $("#hide").click(function(){
+	// 	$("").hide();
+	//   });
+	  
+	//   $("#show").click(function(){
+	// 	$("p").show();
+	//   });
+
+	// OFI Browser
+	objectFitImages();
+});
